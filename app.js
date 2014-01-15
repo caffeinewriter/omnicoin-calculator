0531198c96afad65e4b4ca632e11dc39fb66d797
Brandon Anzaldi <brandon@brandonanzaldi.com>
2014-01-15 11:47:59 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var request = require('request');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico'))); 
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function(req,res) {
+	request({uri: 'http://omc.chains.doctor-blue.net/chain/Omnicoin/q/getdifficulty'}, function(err, resp, body) {
+    	res.render('omc', { difficulty: body });
+  	});
+  //res.render('omc', { difficulty: difficulty }); 
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
